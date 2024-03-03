d61ed493eb6f752dd3d15dae35b9ee57ed80d49a
Frostbowl <masclet.c@gmail.com>
2024-03-03 21:39:24 +0400

    modif favicon

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,8 +3,21 @@
 </template>
 
 <script setup>
+/*
+import { DefinProps, DefinEmits, Ref } from "vue";
+import { onClickOutside } from "@vueuse/core";
+
+const props = DefinProps({
+    isOpen: Boolean,
+});
+
+const emit = DefinEmits(["modal-close"]);
+
+const target = ref(null)
+onClickOutside(target, ()=>emit('modal-close'))
 
 </script>
+
 
 <style scoped>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,14 @@ import Main from '../components/Main.vue';
 import Footer from '../components/Footer.vue';
 import carrousel from '@/components/carrousel.vue';
 import Contact from '@/components/Contact.vue';
+/*import Modal from '../components/Modal.vue';*/
 </script>
 
 <template>
     <Header />
     <Main />
     <carrousel />
+    <!--<modal />-->
     <Contact />
     <Footer />
 </template>
